d8b14450a76eb73faaa0c391be5444cf26f4f26d
Ana Maria Rangel <anamariarangelmarketing@gmail.com>
2025-07-23 17:52:58 -0300

    fixing app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import EnglishLandingPage from './pages/EnglishLandingpage'
+import React from 'react';
+import EnglishLandingPage from './pages/EnglishLandingpage';
+import './assets/styles/global.scss';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <EnglishLandingPage />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
