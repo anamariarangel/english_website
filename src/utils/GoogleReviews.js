78cfc2091edabef772cd12a0eee00607f7acd78c
Ana Maria Rangel <anamariarangelmarketing@gmail.com>
2025-07-23 20:34:51 -0300

    seting api after setting domain

--- src/utils/GoogleReviews.js
+++ src/utils/GoogleReviews.js
@@ -6,7 +6,7 @@ export async function fetchGoogleReviews() {
     // Para produção, use sua URL do Netlify
     const baseUrl = process.env.NODE_ENV === 'development' 
       ? 'http://localhost:8888' 
-      : 'https://68814c929b1cd200081f40f2--englishwe.netlify.app';
+      : 'https://teacherisadora.com.br';;
     
     const response = await fetch(`${baseUrl}/.netlify/functions/place-details?place_id=${PLACE_ID}`);
     
