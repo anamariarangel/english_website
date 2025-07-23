a2d8e8ecf7e1f26e8b7a3852be1ce46ab73e18fc
Ana Maria Rangel <anamariarangelmarketing@gmail.com>
2025-07-23 20:48:39 -0300

    fixing netlify.toml

--- src/utils/GoogleReviews.js
+++ src/utils/GoogleReviews.js
@@ -2,17 +2,14 @@ const PLACE_ID = 'ChIJI_1Dn1FDlpQR51FKnrXqJtM';
 
 export async function fetchGoogleReviews() {
   try {
-    // Para desenvolvimento local, use http://localhost:8888
-    // Para produção, use sua URL do Netlify
-    const baseUrl = process.env.NODE_ENV === 'development' 
-      ? 'http://localhost:8888' 
-      : 'https://teacherisadora.com.br';;
-    
-    const response = await fetch(`${baseUrl}/.netlify/functions/place-details?place_id=${PLACE_ID}`);
+    const response = await fetch(`/.netlify/functions/place-details?place_id=${PLACE_ID}`);
     
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
     
-    return await response.json();
+    const text = await response.text(); // Primeiro capture como texto
+    console.log("Resposta bruta:", text); // Depure aqui
+    
+    return JSON.parse(text); // Depois converta
   } catch (error) {
     console.error('Erro ao buscar reviews:', error);
     throw error;
@@ -24,7 +21,7 @@ export async function fetchPlaces(input) {
   try {
     const baseUrl = process.env.NODE_ENV === 'development' 
       ? 'http://localhost:8888' 
-      : 'https://68814c929b1cd200081f40f2--englishwe.netlify.app';
+      : 'https://teacherisadora.com.br';
     
     const response = await fetch(`${baseUrl}/.netlify/functions/places?input=${encodeURIComponent(input)}`);
     
