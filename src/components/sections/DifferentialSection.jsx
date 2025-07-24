0e1489f17bd188492959dcf319e3836e44d2f13f
Ana Maria Rangel <anamariarangelmarketing@gmail.com>
2025-07-24 18:24:21 -0300

    spliting scss responsivo

--- src/components/sections/DifferentialSection.jsx
+++ src/components/sections/DifferentialSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CTAButton from '../comon/CTAButton';
 import { differentialsData } from '../../data/content';
+import '../../assets/styles/differentials.scss'
 
 const DifferentialItem = ({ icon, title, text }) => (
   <div className="differentials__item">
